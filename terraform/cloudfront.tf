94d15d650b489f5fda7a34d9fba8b6c14ab6be61
NAGATA Hiroaki <nagata@handlena.me>
2025-02-09 02:14:14 +0900

    fix: minimum_protocol_version must be TLSv1
    if using cloudfront_default_certificate

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -11,8 +11,7 @@ resource "aws_cloudfront_distribution" "main" {
 
   viewer_certificate {
     cloudfront_default_certificate = true
-    ssl_support_method             = "sni-only"
-    minimum_protocol_version       = "TLSv1.2_2021"
+    minimum_protocol_version       = "TLSv1"
   }
 
   origin {
